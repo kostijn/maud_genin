52f2908372032c0d968c711accc368ca0c9c83b6
Kilian Ostijn <kilian.ostijn@gmail.com>
2026-03-12 20:01:57 +0100

    add gh pages package

--- maud-portfolio/vite.config.js
+++ maud-portfolio/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // BELANGRIJK: Vervang dit door de exacte naam van je GitHub repo
+  base: '/boobi_portfolio/', 
   plugins: [
     react(),
-    tailwindcss()],
+    tailwindcss(),
+  ],
 })
